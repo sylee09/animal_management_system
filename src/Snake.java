fc49b1e8987a79b04b780dfd392b9b42bbadaf28
Seungyoung Lee <leesy960909@gmail.com>
2025-11-19 15:01:35 +0900

    뱀 구현

---
+++ src/Snake.java
@@ -0,0 +1,35 @@
+public class Snake extends Reptile {
+    private String species;
+
+    public Snake(String name, int age) {
+        super(name, age);
+        this.species = "뱀";
+    }
+
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+
+    @Override
+    public void sound() {
+        System.out.println(getName() + "가 스으하고 소리를 냅니다.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("name = " + getName() + " ");
+        sb.append("age = " + getAge() + " ");
+        sb.append("hungryStat = " + getHungryStat() + " ");
+        sb.append("happinessStat = " + getHappinessStat() + " ");
+        sb.append("species = " + getSpecies() + "\n");
+        return sb.toString();
+    }
+}
