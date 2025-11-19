c258b77ff9bc40a4b41108600b9b869783992a3c
Seungyoung Lee <leesy960909@gmail.com>
2025-11-19 14:56:46 +0900

    펭귄 구현

---
+++ src/Penguin.java
@@ -0,0 +1,40 @@
+public class Penguin extends Bird implements Swimmable {
+    private String species;
+
+    public Penguin(String name, int age) {
+        super(name, age);
+        this.species = "펭귄";
+    }
+
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + "가 헤엄을 칩니다! 행복도가 크게 증가했습니다.");
+        int newHappinessStat = Math.min(100, getHappinessStat() + 10);
+        setHappinessStat(newHappinessStat);    }
+
+    @Override
+    public void sound() {
+        System.out.println(getName() + "가 끼룩하고 소리를 냅니다.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("name = " + getName() + " ");
+        sb.append("age = " + getAge() + " ");
+        sb.append("hungryStat = " + getHungryStat() + " ");
+        sb.append("happinessStat = " + getHappinessStat() + " ");
+        sb.append("species = " + getSpecies() + "\n");
+        return sb.toString();
+    }
+}
