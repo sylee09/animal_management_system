38effeabe514a54352dc790c0fa6cd5574d42304
Seungyoung Lee <leesy960909@gmail.com>
2025-11-19 14:58:24 +0900

    사자 기능 구현

---
+++ src/Lion.java
@@ -0,0 +1,34 @@
+public class Lion extends Mammal {
+    private String species;
+
+    public Lion(String name, int age) {
+        super(name, age);
+        this.species = "사자";
+    }
+
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println(getName() + "가 크르릉하고 소리를 냅니다.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("name = " + getName() + " ");
+        sb.append("age = " + getAge() + " ");
+        sb.append("hungryStat = " + getHungryStat() + " ");
+        sb.append("happinessStat = " + getHappinessStat() + " ");
+        sb.append("species = " + getSpecies() + "\n");
+        return sb.toString();
+    }
+}
