8657a77fa99acce9c311be861eedaae281b8b543
Seungyoung Lee <leesy960909@gmail.com>
2025-11-19 15:02:26 +0900

    거북이 구현

---
+++ src/Turtle.java
@@ -0,0 +1,35 @@
+public class Turtle extends Reptile {
+    private String species;
+
+    public Turtle(String name, int age) {
+        super(name, age);
+        this.species = "거북이";
+    }
+
+    @Override
+    public String getSpecies() {
+        return species;
+    }
+
+    @Override
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+
+    @Override
+    public void sound() {
+        System.out.println(getName() + "가 거북하고 소리를 냅니다.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("name = " + getName() + " ");
+        sb.append("age = " + getAge() + " ");
+        sb.append("hungryStat = " + getHungryStat() + " ");
+        sb.append("happinessStat = " + getHappinessStat() + " ");
+        sb.append("species = " + getSpecies() + "\n");
+        return sb.toString();
+    }
+}
